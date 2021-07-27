3de84aa71787dd3a215ff16fd3747886a395e65a
rexyfahrezi <rexyfahrezi30@gmail.com>
2021-07-27 14:01:47 -0700

    notes-app

---
+++ notes-app-back-end/src/routes.js
@@ -0,0 +1,16 @@
+const {addNoteHandler} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: '/GET',
+    path: '/notes',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
